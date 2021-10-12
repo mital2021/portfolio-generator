f1cb69303ec4dd1d1592ea943579e0c6727bafd6
mital2021 <87838938+mital2021@users.noreply.github.com>
2021-10-12 19:25:50 -0400

    second commit

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 
   const profileDataArgs = process.argv.slice(2);
 
@@ -15,11 +15,4 @@ const printProfileData = profileDataArr => {
 };
 
 printProfileData(profileDataArgs);
-=======
-var message = 'Hello Node!';
 
-var sum = 5 + 3;
-
-console.log(message);
-console.log(sum);
->>>>>>> 6ff97069f724d479ac524fd4ff875c29df9b2878
