c3c95a530e38c7468020c5c526cb60d26333a23d
mital2021 <87838938+mital2021@users.noreply.github.com>
2021-10-12 20:57:26 -0400

    add html

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
+const fs = require('fs');
+
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
 
 
-  const profileDataArgs = process.argv.slice(2);
+fs.writeFile('./index.html', generatePage('Mital', 'github'), err => {
+  if (err) throw new Error(err);
 
-const printProfileData = profileDataArr => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
-
-  console.log('================');
-
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs);
-
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
